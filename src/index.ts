9f84086d87927cdf2447fe4c8ce27e4b9f8d638b
Hector Maddock-Greene <hector.maddock-gree@wrenkitchens.com>
2021-03-28 12:30:27 +0100

    Moved deep comparison functionality into a separate library

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { equals } from "deep-cmp";
+
 interface ReportCount {
     passed: number;
     failed: number;
@@ -27,63 +29,14 @@ export function assertNeq(left: primitive, right: primitive): void {
     }
 }
 
-function areDeepEq(startLeft: any, startRight: any): boolean {
-    const seenObjects = new WeakSet<object>();
-    const toProcess = [[startLeft, startRight]];
-
-    while (toProcess.length > 0) {
-        const [left, right] = toProcess.pop()!;
-
-        const bothAreTheSame = Object.is(left, right);
-
-        if (!bothAreTheSame) {
-            // If they're not both the same, then we need to check that they're both objects because two different
-            // objects with identical properties don't compare as equal (They're references to different memory).
-            const leftIsObejct = typeof left === "object" && left !== null;
-            const rightIsObject = typeof right === "object" && right !== null;
-            const bothAreObjects = leftIsObejct && rightIsObject;
-
-            if (bothAreObjects) {
-                // If they're both objects, then we need to check if we've seen them before.
-                const seenLeft = seenObjects.has(left);
-                const seenRight = seenObjects.has(right);
-
-                if (seenLeft && seenRight) {
-                    // If we've seen them before, then we've already queued up their properties.
-                    continue;
-                } else if (seenLeft || seenRight) {
-                    // If we've seen one, but not the other then these objects are different.
-                    return false;
-                } else {
-                    // If we haven't seen either of them before, then we should queue up their
-                    // properties and mark them as seen.
-                    for (const key of Object.keys(left)) {
-                        const newLeft = left[key];
-                        const newRight = right[key];
-                        toProcess.push([newLeft, newRight]);
-                    }
-
-                    seenObjects.add(left);
-                    seenObjects.add(right);
-                }
-            } else {
-                // If they're not the same and they're not objects, then startLeft and startRight are different.
-                return false;
-            }
-        }
-    }
-
-    return true;
-}
-
 export function assertDeepEq(left: any, right: any): void {
-    if (!areDeepEq(left, right)) {
+    if (!equals(left, right)) {
         throw new Error(`expected ${left.toString()} to equal ${right.toString()}`);
     }
 }
 
 export function assertDeepNeq(left: any, right: any): void {
-    if (areDeepEq(left, right)) {
+    if (equals(left, right)) {
         throw new Error(`expected ${left.toString()} to equal ${right.toString()}`);
     }
 }
